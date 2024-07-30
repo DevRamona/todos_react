d7e7393516a51546c81a80b52d8dd1e66ec418c6
DevRamona <r.ingabire@alustudent.com>
2024-07-30 14:54:44 +0200

    updates

--- vite-project/src/Components/Card.jsx
+++ vite-project/src/Components/Card.jsx
@@ -8,18 +8,25 @@ const Card = (props) => {
     function checker() {
         setChecked(!checked)
     }
+
+    const styles = {
+        textDecoration :"line-through",
+        color : "red"
+    }
     return (
         <section className="mt-4 flex justify-between">
             <div className="flex items-center">
             <input className=""
             type="checkbox"
             onChange = {checker}/>
-            <p className="text-2xl">{props.text}</p>
+            <p className="text-2xl">
+                style = {checked ? styles : {}}
+                {props.text}</p>
         </div>
 
         <img src="./trash-solid.svg"
         onClick={props.delete}
-        className="w-12 border-2  text-red-500 rounded-full p-2"
+        className="w-12 border-2 text-red-500 rounded-full p-2"
         alt="trash-icon"
         name = {props.id}
          />
